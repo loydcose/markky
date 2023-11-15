5afbc5d1bde3109fa25ea1228b60e89499088753
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-11-15 18:23:37 +0800

    Add delete functionality

--- components/note-list.tsx
+++ components/note-list.tsx
@@ -4,9 +4,25 @@ import React from "react"
 import { Button } from "./ui/button"
 import { X } from "lucide-react"
 import { useNotesStore } from "@/slices/use-notes-store"
+import { notes } from "@/data"
 
 export default function NoteList({ note }: { note: any }) {
   const { setNoteId, noteId } = useNotesStore()
+
+  const handleDeleteNote = () => {
+    for (let i = 0; i < notes.length; i++) {
+      if (notes[i].id === noteId) {
+        notes.splice(i, 1)
+        break
+      }
+    }
+    // const sortedNotes = [...notes].sort(
+    //   (a, b) =>
+    //     new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+    // )
+    // console.log(sortedNotes[0].id)
+    // setNoteId(sortedNotes[0].id)
+  }
 
   return (
     <Button
@@ -16,7 +32,11 @@ export default function NoteList({ note }: { note: any }) {
       className="flex items-center justify-between gap-2 group text-ellipsis"
     >
       <p>{note.title}</p>
-      <Button variant={"ghost"} className="opacity-0 group-hover:opacity-100">
+      <Button
+        onClick={handleDeleteNote}
+        variant={"ghost"}
+        className="opacity-0 group-hover:opacity-100"
+      >
         <X size={20} />
       </Button>
     </Button>
