c009bfc92ca9e5e8e3d94619db8d31c36e288123
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-11-15 18:08:03 +0800

    Add selected note UI

--- components/note-list.tsx
+++ components/note-list.tsx
@@ -6,13 +6,13 @@ import { X } from "lucide-react"
 import { useNotesStore } from "@/slices/use-notes-store"
 
 export default function NoteList({ note }: { note: any }) {
-  const { setNoteId } = useNotesStore()
+  const { setNoteId, noteId } = useNotesStore()
 
   return (
     <Button
       onClick={() => setNoteId(note.id)}
       key={note.id}
-      variant={"outline"}
+      variant={noteId === note.id ? "default" : "outline"}
       className="flex items-center justify-between gap-2 group text-ellipsis"
     >
       <p>{note.title}</p>
