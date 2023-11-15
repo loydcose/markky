a3218acf4ae982ca499f4040727990aa4c210ce9
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-11-15 17:54:39 +0800

    Added add-note functionality

--- components/side-panel.tsx
+++ components/side-panel.tsx
@@ -1,10 +1,28 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { notes } from "@/data"
 import { PanelLeftClose, X, LogOut, Github } from "lucide-react"
 import React from "react"
 import NoteList from "./note-list"
+import { useNotesStore } from "@/slices/use-notes-store"
 
 export default function SidePanel({ className }: { className?: string }) {
+  const { setNoteId } = useNotesStore()
+
+  const handleCreateNew = () => {
+    const id = notes.length + 1
+    const prop = {
+      id,
+      title: "Untitled",
+      content: "",
+      createdAt: new Date().toString(),
+      updatedAt: new Date().toString(),
+    }
+    notes.push(prop)
+    setNoteId(id)
+  }
+
   return (
     <aside className={"flex flex-col " + className}>
       <div className="flex items-start justify-between mb-6">
@@ -13,14 +31,18 @@ export default function SidePanel({ className }: { className?: string }) {
           <PanelLeftClose size={20} />
         </Button>
       </div>
-      <Button variant={"default"} className="w-full mb-10">
+      <Button
+        onClick={handleCreateNew}
+        variant={"default"}
+        className="w-full mb-10"
+      >
         Create new
       </Button>
 
       <h2 className="mb-2">Notes</h2>
       <div className="flex flex-col gap-2 mb-10">
         {notes.map((note) => (
-          <NoteList key={note.id} note={note}/>
+          <NoteList key={note.id} note={note} />
         ))}
       </div>
 
