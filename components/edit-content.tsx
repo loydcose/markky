e4109f9518442d5f7da6565d84969ebf08d80610
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-11-15 18:07:02 +0800

    Add update notes functionality

--- components/edit-content.tsx
+++ components/edit-content.tsx
@@ -4,14 +4,34 @@ import React, { useState, useEffect } from "react"
 import { useNotesStore } from "@/slices/use-notes-store"
 import { notes } from "@/data"
 import DynamicTextarea from "./dynamic-textarea"
+import { useDebounce } from "@/hooks/use-debounce"
 
 export default function EditContent() {
   const { noteId } = useNotesStore()
   const [input, setInput] = useState("")
+  const debouncedValue = useDebounce<string>(input, 1000)
+  const foundNote = notes.find((note) => noteId === note.id)
 
   useEffect(() => {
-    setInput(notes.find((note) => noteId === note.id)?.content || "")
+    setInput(foundNote?.content || "")
   }, [noteId])
+
+  useEffect(() => {
+    if (!foundNote) return
+
+    for (const noteItem of notes) {
+      const {id, title, createdAt, updatedAt} = foundNote
+
+      if (noteItem.id === noteId) {
+        noteItem.id = id
+        noteItem.title = title
+        noteItem.content = input
+        noteItem.createdAt = createdAt
+        noteItem.updatedAt = updatedAt
+      }
+    }
+    console.log("updated")
+  }, [debouncedValue])
 
   return (
     <DynamicTextarea
