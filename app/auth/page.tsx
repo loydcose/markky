85b97fa74fc9ea60ebbce0d6af054690a0c9d687
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-11-18 02:03:31 +0800

    Add redirect for login page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,13 @@
+import { getServerSession } from "next-auth"
 import Auth from "./auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(authOptions)
+  if (session) {
+    redirect("/")
+  }
+
   return <Auth />
 }
