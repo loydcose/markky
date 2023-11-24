4191049e0995f5a523bf56a7043cff334cf66442
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-11-24 20:18:54 +0800

    Add validations & functionality

--- components/edit-note-title.tsx
+++ components/edit-note-title.tsx
@@ -1,13 +1,37 @@
 "use client"
 
+import { getUserNotes, updateNote } from "@/actions"
+import { useDebounce } from "@/hooks/use-debounce"
 import { useNotesStore } from "@/slices/use-notes-store"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function EditNoteTitle() {
-  const { selectedNote } = useNotesStore()
+  const { selectedNote, user, setUserNotes } = useNotesStore()
   const [input, setInput] = useState(selectedNote?.title || "Untitled")
+  const debouncedValue = useDebounce<string>(input, 1000)
 
-  
+  useEffect(() => {
+    setInput(selectedNote?.title || "")
+  }, [selectedNote])
+
+  const updateContent = async () => {
+    if (!selectedNote) return
+    if (!input.trim()) return
+
+    try {
+      await updateNote(selectedNote._id, { title: input })
+
+      // refresh fetch? that's a bad idea literally. But let's try
+      const userNotes = await getUserNotes(user?._id || "")
+      setUserNotes(userNotes)
+    } catch (error: any) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    updateContent()
+  }, [debouncedValue])
 
   return (
     <div className="w-full mx-auto max-w-[700px]">
